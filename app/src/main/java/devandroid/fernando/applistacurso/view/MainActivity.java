a4a9821b8393806d7d8fdaefff86e6e36a4c1379
Fernando <nando.a.g.martines@gmail.com>
2024-05-08 19:56:44 -0300

    Arquitetura MVC

--- app/src/main/java/devandroid/fernando/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/fernando/applistacurso/view/MainActivity.java
@@ -1,4 +1,4 @@
-package devandroid.fernando.applistacurso;
+package devandroid.fernando.applistacurso.view;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import devandroid.fernando.applistacurso.R;
 
 public class MainActivity extends AppCompatActivity {
 
